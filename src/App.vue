0cc490d6817ffc97691b24709a3c000da9559e16
James <jamescai800@gmail.com>
2024-11-26 20:51:03 -0500

    Changed app.vue tempaltes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HomeView from './views/HomeView.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-
-<HomeView/>
-
+  <Suspense>
+    <RouterView />
+  </Suspense>
 </template>
 
 <style scoped></style>
